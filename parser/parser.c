98c338e4e58a4810b22de3c503602d3c2d676cc8
rathesh77 <rathesh.charles@outlook.fr>
2022-05-20 12:09:13 +0200

    add plan

--- parser/parser.c
+++ parser/parser.c
@@ -3,6 +3,15 @@
 
 void parse_code(buffer_t *buffer) {
 
+  // presence d'accolades
+  // declarations de fonctions (function func (a : entier, b: entier ...) {})
+  // appels de fonctions ( func(a, b) )
+  // boucle (for while, do while)
+  // condition si else if else ...
+  // affectations  (entier a = 5)
+  // instructions composés ({ a = 2;b = 3; })
+  // ne pas oublier les operateurs unaires (++, --, *=, %=, /=, ...)
+
   while (!buf_eof(buffer)) {
     char s = buf_getchar(buffer);
     if ( s == ' ') {
